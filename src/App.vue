1bf0aa4bad77076d20901aba62ee0406f8b39f5a
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-11-30 13:18:24 -0500

    Write starting page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Lobby />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Lobby from './components/Lobby.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    Lobby
+  },
 }
 </script>
 
---
+++ src/components/Lobby.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Who Wants to Be a Bitcoinnaire?</h1>
+
+    <div class="lobby__game-options">
+      <label for="difficulty-select">
+        Difficulty
+      </label>
+
+      <select id="difficulty-select">
+        <option
+          v-for="difficulty in difficulties"
+          :key="difficulty.id"
+          :value="difficulty.id"
+        >
+          {{ difficulty.label }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lobby',
+
+  data() {
+    return {
+      difficulties: [
+        { id: 'easy', label: 'Easy' },
+        { id: 'medium', label: 'Medium' },
+        { id: 'hard', label: 'Hard' },
+      ],
+    };
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
