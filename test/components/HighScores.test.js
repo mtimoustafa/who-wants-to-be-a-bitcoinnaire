830239e57ad15edfd989c7e42a2ed3afbc123191
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-12-06 10:32:07 -0500

    Add component tests

--- test/components/HighScores.test.js
+++ test/components/HighScores.test.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'; // shallowMount is not yet available in Vue Test Utils 2
 import { createStore } from 'vuex';
 import Store from '../../src/store/store'; // Vue Test Utils 2 doesn't like store mocks yet either
-import Questions from '../../src/views/Questions.vue';
+import HighScores from '../../src/components/HighScores.vue';
 
 let wrapper;
 
@@ -11,8 +11,8 @@ const mockRouter = {
 
 const store = createStore(Store);
 
-beforeEach(() => {
-  wrapper = mount(Questions, {
+const mountWrapper = () => {
+  wrapper = mount(HighScores, {
     global: {
       plugins: [store],
       mocks: {
@@ -20,12 +20,68 @@ beforeEach(() => {
       },
     },
   });
-});
+};
 
-describe('Questions', () => {
-  it('displays placeholder message if no high scores found', () => {
+describe('HighScores', () => {
+  it('shows correct columns', () => {
+    mountWrapper();
+
+    const headers = wrapper.findAll('th');
+    expect(headers.map(h => h.text())).toEqual(
+      expect.arrayContaining([
+        'Name',
+        'Score',
+        'Accuracy',
+        'Difficulty',
+      ])
+    );
   });
 
-  it('displays high scores', () => {
+  it('displays placeholder message if no high scores found', () => {
+    mountWrapper();
+
+    // Only header and row with placeholder message should show
+    expect(wrapper.findAll('tr').length).toEqual(2);
+    expect(wrapper.text()).toContain('No high scores...yet!');
+  });
+
+  describe('high scores present', () => {
+    beforeEach(() => {
+      store.state.highScores = [
+        {
+          playerName: 'Test 1',
+          score: 5,
+          totalQuestions: 10,
+          rankingScore: 50,
+          difficulty: 'easy',
+        },
+        {
+          playerName: 'Test 2',
+          score: 4,
+          totalQuestions: 10,
+          rankingScore: 40,
+          difficulty: 'medium',
+        },
+      ];
+
+      mountWrapper();
+    });
+
+    it('displays correct number of scores', () => {
+      const [ header, ...scores ] = wrapper.findAll('tr');
+      expect(scores.length).toEqual(2);
+    });
+
+    it('displays score correctly', () => {
+      const [ header, score1, score2 ] = wrapper.findAll('tr');
+      const scoreDetails = score1.findAll('td');
+
+      const expectedScore = `${store.state.highScores[0].score} of ${store.state.highScores[0].totalQuestions}`;
+
+      expect(scoreDetails[0].text()).toEqual(store.state.highScores[0].playerName);
+      expect(scoreDetails[1].text()).toEqual(expectedScore);
+      expect(scoreDetails[2].text()).toEqual(`${store.state.highScores[0].rankingScore}%`);
+      expect(scoreDetails[3].text()).toEqual('Easy');
+    });
   });
 });
